f0b3bc3271a392d8c79e03dbb4f131c102b9291d
TaeYongJung <1977974235@qq.com>
2026-08-27 22:00:34 +0800

    机会列表支持首页入口分类筛选

--- pages/list/list.js
+++ pages/list/list.js
@@ -1,20 +1,31 @@
-const cards = require('../../data/cards')
+const allCards = require('../../data/cards')
 
 Page({
   data: {
-    cards,
-    keyword: ''
+    cards: [],
+    keyword: '',
+    currentType: ''
   },
+
+  onLoad(options) {
+    const type = options.type || ''
+    const cards = type ? allCards.filter(item => item.type.includes(type) || item.category === type) : allCards
+    this.setData({ cards, currentType:type })
+  },
+
   search(e) {
     const keyword = e.detail.value
-    this.setData({
-      keyword,
-      cards: cards.filter(item => item.title.includes(keyword) || item.type.includes(keyword))
-    })
+    const cards = allCards.filter(item =>
+      item.name.includes(keyword) ||
+      item.type.includes(keyword) ||
+      item.location.includes(keyword)
+    )
+    this.setData({ keyword, cards })
   },
+
   openDetail(e) {
     wx.navigateTo({
-      url: '/pages/detail/detail?id=' + e.currentTarget.dataset.id
+      url:'/pages/detail/detail?id=' + e.currentTarget.dataset.id
     })
   }
 })
