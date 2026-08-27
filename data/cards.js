8aedfaed8b8dcfead8e81de1e56db76396837cb8
TaeYongJung <1977974235@qq.com>
2026-08-27 23:17:26 +0800

    add data folder with cards data file

---
+++ data/cards.js
@@ -0,0 +1,20 @@
+const cards = [
+  {
+    id: 1,
+    title: "百源康鱼菜共生实践基地",
+    type: "实践站点",
+    location: "洛舍镇",
+    desc: "农业实践、生态农业调研、项目参与",
+    status: "长期有效"
+  },
+  {
+    id: 2,
+    title: "浙江立金金属科技有限公司",
+    type: "实习/就业",
+    location: "德清开发区",
+    desc: "企业实习、岗位体验",
+    status: "招聘中"
+  }
+]
+
+module.exports = cards
