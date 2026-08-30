d5f50d36ee9d74fadf078f676e88bbc210b34203
TaeYongJung <1977974235@qq.com>
2026-08-30 10:38:51 +0800

    为咨询单位详情页增加独立联系逻辑

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,8 +1,30 @@
 const cards = require('../../data/cards')
+
 Page({
- data:{item:{}},
- onLoad(options){
-  const item=cards.find(i=>i.id==options.id)
-  this.setData({item:item||{}})
- }
+  data: {
+    item: {},
+    isContact: false
+  },
+
+  onLoad(options) {
+    const item = cards.find(i => i.id == options.id) || {}
+    this.setData({
+      item,
+      isContact: item.type === '咨询单位'
+    })
+  },
+
+  callPhone() {
+    const phone = this.data.item.phone || ''
+    if (/^\d[\d-]*$/.test(phone)) {
+      wx.makePhoneCall({
+        phoneNumber: phone.replace(/-/g, '')
+      })
+    } else {
+      wx.showToast({
+        title: '请通过德清县人民政府官网查询',
+        icon: 'none'
+      })
+    }
+  }
 })
